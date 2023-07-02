4759680134ac24581fbca932f883aad19ad13021
Radostin Dimitrov <73715258+terminator2609@users.noreply.github.com>
2023-07-02 18:24:29 +0300

    add firebase host

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCw9JM8xH3Fz_aN6Fcri3TPn5Xr88K_HYs",
+  authDomain: "fitbox-d8751.firebaseapp.com",
+  projectId: "fitbox-d8751",
+  storageBucket: "fitbox-d8751.appspot.com",
+  messagingSenderId: "584382510695",
+  appId: "1:584382510695:web:2a040a5f611baecf0406c2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
